c54add182e3e3908cc3bf2c1f572fc51294d6f91
Hitsujii <dev.hitsuji@gmail.com>
2026-05-23 20:42:21 +0200

    chore(template): use public NextPaper defaults

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Documentation Site',
+    description: `A small documentation-style project entry. Replace this with your own project,
+    product or case study when you customize the template.`,
+    href: '/blog/introducing-tailwind-nextjs-starter-blog',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Content-Driven Blog',
+    description: `An example project showing how NextPaper can be used for MDX articles, tags,
+    RSS, sitemap and SEO-friendly static publishing.`,
+    href: '/blog/github-markdown-guide',
   },
 ]
 
