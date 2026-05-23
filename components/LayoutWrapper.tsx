b5c7a88060f0a0e133692ddc791ca7b0c9810f00
Hitsujii <dev.hitsuji@gmail.com>
2026-05-23 22:52:36 +0200

    fix(theme): finalize AstroPaper visual baseline

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,27 +1,18 @@
-import { Inter } from 'next/font/google'
-import SectionContainer from './SectionContainer'
-import Footer from './Footer'
 import { ReactNode } from 'react'
+import Footer from './Footer'
 import Header from './Header'
+import SectionContainer from './SectionContainer'
 
 interface Props {
   children: ReactNode
 }
 
-const inter = Inter({
-  subsets: ['latin'],
-})
-
-const LayoutWrapper = ({ children }: Props) => {
+export default function LayoutWrapper({ children }: Props) {
   return (
-    <SectionContainer>
-      <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
-        <Header />
-        <main className="mb-auto">{children}</main>
-        <Footer />
-      </div>
-    </SectionContainer>
+    <>
+      <Header />
+      <SectionContainer>{children}</SectionContainer>
+      <Footer />
+    </>
   )
 }
-
-export default LayoutWrapper
