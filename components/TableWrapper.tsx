d70c1b4765265b69e1c942f82ab7f653c43cfba7
Hitsujii <dev.hitsuji@gmail.com>
2026-05-23 20:38:58 +0200

    style(typography): match AstroPaper markdown content

--- components/TableWrapper.tsx
+++ components/TableWrapper.tsx
@@ -6,8 +6,10 @@ type Props = {
 
 export default function TableWrapper({ children }: Props) {
   return (
-    <div className="overflow-hidden">
-      <div className="relative w-full overflow-x-auto">{children}</div>
+    <div className="overflow-hidden [&_table]:my-0 [&_table]:min-w-xl">
+      <div className="relative w-full overflow-x-auto">
+        <table>{children}</table>
+      </div>
     </div>
   )
 }
